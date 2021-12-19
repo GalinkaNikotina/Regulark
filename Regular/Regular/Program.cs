fe62317730ce27d1f0095b5ba708120f94595384
Galinka <galinka2002niko@gmail.com>
2021-12-20 00:08:34 +0300

    ty

---
+++ Regular/Regular/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Regular
+{
+    class Program
+    {
+        static void Main()
+        {
+            string pattern = "(Mr\\.? |Mrs\\.? |Miss |Ms\\.? )";
+            string[] names = { "Mr. Henry Hunt", "Ms. Sara Samuels",
+                         "Abraham Adams", "Ms. Nicole Norris" };
+            foreach (string name in names)
+                Console.WriteLine(Regex.Replace(name, pattern, String.Empty));
+        }
+    }
+}
